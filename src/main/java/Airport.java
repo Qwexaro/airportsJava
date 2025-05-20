bd336fceb8ecf33a644c9cc5e2a573a60862c486
Qwexaro <Sergikstar25.gold@mail.ru>
2025-05-20 12:22:52 +0300

    Main

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -9,35 +9,35 @@ import java.util.TreeMap;
 
 public class Airport {
     private final String urlToMainPageAirports = "https://www.aviasales.ru";
-    private Map<String, String>mapAllAirports;
+    private Map<String, String> mapAllAirports;
     private final String patchToFilesHtml = "src/main/resources/data/";
 
-    public Airport(){
+    public Airport() {
         mapAllAirports = new TreeMap<>();
         fillMapAllAirports();
     }
 
-    public Document returnHtmlPage(String urlPage){
+    public Document returnHtmlPage(String urlPage) {
         Document document = null;
         try {
             document = Jsoup.connect(urlPage).get();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.getMessage();
         }
         return document;
     }
 
     //TODO возвращение map всех аэропортов
-    public void fillMapAllAirports(){
+    public void fillMapAllAirports() {
         try {
             Document document = returnHtmlPage(urlToMainPageAirports);
             FileWriter fileWriter = new FileWriter(patchToFilesHtml + "Главная Страница.html");
             fileWriter.write(document.toString());
 
             Elements elements = document.select(".s__aMgHXZZASaSwDP9wIMPx.s__VxY6BmkE3QOJIwvomxXQ.s__Z6pCHtb9k0byQSELy7Av");
-            for (Element element : elements){
+            for (Element element : elements) {
                 String strElement = element.toString();
-                if(strElement.indexOf("href=\"/airports\"") != -1){
+                if (strElement.indexOf("href=\"/airports\"") != -1) {
                     String templateForLinkAllAirports = "href=\"";
                     int startIndexForLinkAllAirports = strElement.indexOf(templateForLinkAllAirports) + templateForLinkAllAirports.length();
                     int endIndexForLinkAllAirports = strElement.indexOf("\"", startIndexForLinkAllAirports);
@@ -48,16 +48,15 @@ public class Airport {
                     fileWriterForAllAirports.write(documentForAllAirports.toString());
 
                     Elements elementsForAllAirports = documentForAllAirports.select(".index-list__item.is-active");
-                    for(Element elementsForAllAirport : elementsForAllAirports) {
+                    for (Element elementsForAllAirport : elementsForAllAirports) {
                         System.out.println("\uD83C\uDCCF" + elementsForAllAirport + "\uD83C\uDCCF");
                     }
                 }
             }
-        }catch (Exception ex){
+        } catch (Exception ex) {
             ex.getMessage();
         }
     }
-
 
 
 }
